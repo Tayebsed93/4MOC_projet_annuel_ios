eb599739b7d45bb6f3e295847cb97c8220bc8e82
Tayeb Sedraia <tayebsedraia@MacBook-Pro-de-Tayeb.local>
2017-04-28 18:05:09 +0200

    creation projet

---
+++ PoubelleConnecte/PoubelleConnecte/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PoubelleConnecte
+//
+//  Created by Tayeb Sedraia on 28/04/2017.
+//  Copyright © 2017 Tayeb Sedraia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

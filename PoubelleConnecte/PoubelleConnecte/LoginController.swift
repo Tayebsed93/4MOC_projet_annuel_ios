18be9279a2f595c60fb61e82eb26a63649b7a561
Tayeb Sedraia <tayebsedraia@MacBook-Pro-de-Tayeb.local>
2017-04-28 18:07:24 +0200

    init loginVC

--- PoubelleConnecte/PoubelleConnecte/LoginController.swift
+++ PoubelleConnecte/PoubelleConnecte/LoginController.swift
@@ -1,25 +1,25 @@
 //
-//  ViewController.swift
+//  LoginController.swift
 //  PoubelleConnecte
 //
 //  Created by Tayeb Sedraia on 28/04/2017.
 //  Copyright © 2017 Tayeb Sedraia. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class LoginController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-

c35cfd83b097ba89ec09369022ff49fa583c4395
Tayeb Sedraia <tayebsedraia@MacBook-Pro-de-Tayeb.local>
2017-06-11 19:23:39 +0200

    add api call size : RemplissageController is ok

--- PoubelleConnecte/PoubelleConnecte/RemplissageController.swift
+++ PoubelleConnecte/PoubelleConnecte/RemplissageController.swift
@@ -14,27 +14,53 @@ class RemplissageController: UIViewController {
     @IBOutlet weak var circleProgressView: CircleProgressView!
     @IBOutlet weak var progressLabel: UILabel!
     
-    var passapikey = String()
+    public var addressUrlString = "http://localhost:8888/PoubelleAPI/API/v1"
     
+    public var loginUrlString = "/poubelles/date"
+    
+    var passapikey = String()
+    var nb = Double()
+    
+    var dates = [Int]()
     
     let nf = NumberFormatter()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        nf.numberStyle = NumberFormatter.Style.decimal
-        nf.maximumFractionDigits = 2
-        self.circleProgressView.progress = Double(0.10)
-        //self.progressSlider.value = Float(0.10)
-        let nb = 0.10 * 100
-        self.progressLabel.text = nf.string(from: NSNumber(value: nb))!
+        
+        //Call circular change size 
+        //circularSize(money : [Int])
+        
+
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        callAPIPoubelleSize()
+        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-    // MARK: - IBActions
+    // MARK: - Circular
+    func circularSize(sizes : [Int]) {
+        for (i, element) in sizes.enumerated() {
+            //print("Item \(i): \(element)")
+            if (element > 0) {
+                self.nb = Double(element)
+            }
+        }
+        nf.numberStyle = NumberFormatter.Style.decimal
+        nf.maximumFractionDigits = 2
+
+        let ajust = nb / 100
+        self.circleProgressView.progress = ajust
+        self.progressLabel.text = nf.string(from: NSNumber(value: self.nb))! + " %"
+    }
     
     
     // MARK: - Helpers
@@ -42,5 +68,94 @@ class RemplissageController: UIViewController {
         let delayTime = DispatchTime.now() + delay
         DispatchQueue.main.asyncAfter(deadline: delayTime, execute: closure)
     }
+    
+    func callAPIPoubelleSize() {
+        
+        
+        
+        let apiKey = passapikey
+        let config = URLSessionConfiguration.default
+        let userPasswordString = "username@gmail.com:password"
+        let userPasswordData = userPasswordString.data(using: String.Encoding.utf8)
+        
+        config.httpAdditionalHeaders = ["Authorization" : apiKey]
+        let session = URLSession(configuration: config)
+        
+        var running = false
+        let url = NSURL(string: "http://localhost:8888/PoubelleAPI/API/v1/poubelles")
+        let task = session.dataTask(with: url as! URL) {
+            ( data, response, error) in
+            if let httpResponse = response as? HTTPURLResponse {
+                let dataString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+                
+                //JSONSerialization in Object
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data!, options:.allowFragments) as! [String : AnyObject]
+                    DispatchQueue.main.async()
+                        {
+                            if let poubelles = json["poubelle"] as? [[String: Any]] {
+
+                                for poubelle in poubelles {
+                                    if let size = poubelle["size"]{
+                                        
+                                        self.dates.append(size as! Int)
+                                        
+                                        self.circularSize(sizes: self.dates)
+                                    }
+                                }
+                            }
+                            
+                            if let messageError = json["message"]
+                            {
+                                self.alerteMessage(message: messageError as! String)
+                            }
+                            
+                            
+                            
+                    }
+                    /*
+                     DispatchQueue.main.async() {
+                     self.dismiss(animated: true, completion: nil)
+                     }
+                     */
+                    
+                    
+                } catch let error as NSError {
+                    print("Failed to load: \(error.localizedDescription)")
+                }
+            }
+            running = false
+        }
+        
+        
+        
+        running = true
+        task.resume()
+        
+        while running {
+            //sleep(UInt32(0.1))
+        }
+        
+    }
+    
+    func alerteMessage(message : String) {
+        
+        var newMessage = ""
+        if (message == "Could not connect to the server." ) {
+            newMessage = "Impossible de se connecter au serveur."
+            
+            let alert = UIAlertController(title: "Erreur", message: newMessage, preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+        else {
+            let alert = UIAlertController(title: "", message: message, preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+        
+    }
+
     
 }
